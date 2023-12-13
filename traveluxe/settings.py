3570c91e27089abd2c86342ef321adc01b6a3f49
Liam-WB <>
2023-12-13 20:09:25 +0000

    -Add md_images, remove commented out code

--- traveluxe/settings.py
+++ traveluxe/settings.py
@@ -112,13 +112,6 @@ WSGI_APPLICATION = 'traveluxe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
